fb1d31de9c47a5ba9701779211e03cc2ff19bdab
Akhil <akhil@contaro.com>
2026-06-05 15:08:16 +0200

    Add GitHub Actions workflow for deployment and update Vite config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS
+    ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/`
+    : '/',
   plugins: [react(), tailwindcss()],
 })
